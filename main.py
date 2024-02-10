54a94f8bb0637cc44faae1e919775f36f45bf3ae
Shilpibouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-02-10 19:45:07 +0530

    Day 21:Add description on the new column

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 =st.columns(2)
 
 with col1:
-    st.image("images/photo.png" , width=300)
+    st.image("images/photo.png" , width=400)
 
 with col2:
     st.title("Shilpi Bouddha")
@@ -15,3 +15,8 @@ with col2:
     I am learning advance Python."""
 
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me. """
+
+st.write(content2)
