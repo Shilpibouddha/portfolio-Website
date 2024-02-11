efb01984415aa14448f78f5852a9e7121152830e
Shilpibouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-02-11 21:26:26 +0530

    Day 23:Add navigation Menu and contact us page

--- Home.py
+++ Home.py
@@ -6,10 +6,11 @@ st.set_page_config(layout="wide")
 col1, col2 =st.columns(2)
 
 with col1:
-    st.image("images/photo.png" , width=300)
+    st.image("images/photo.png" , width=200 )
 
 with col2:
     st.title("Shilpi Bouddha")
+
     content ="""
     Hi, I am shilpi! I am a python programmer and a Cyber Security Analyst.
     
@@ -19,6 +20,9 @@ with col2:
 
 content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me. """
+
+
+
 
 st.write(content2)
 
--- StudentProjectDay22/App.py
+++ StudentProjectDay22/App.py
@@ -13,23 +13,23 @@ st.subheader("Our Team")
 
 col1,col2,col3= st.columns(3)
 
-df=pandas.read_csv("data (2).csv")
+df=pandas.read_csv("/Users/shilpibouddha/PycharmProjects/app2-portfolio/StudentProjectDay22/data (2).csv")
 
 with col1:
     for index, row in df[:4].iterrows():
         st.subheader(f"{row["first name"].title()} {row["last name"].title()}")
         st.write(row["role"])
-        st.image("images (2)/"+ row["image"])
+        st.image("/Users/shilpibouddha/PycharmProjects/app2-portfolio/StudentProjectDay22/images (2)/"+ row["image"])
 
 
 with col2:
     for index, row in df[4:8].iterrows():
         st.subheader(f"{row["first name"].title()} {row["last name"].title()}")
         st.write(row["role"])
-        st.image("images (2)/"+ row["image"])
+        st.image("/Users/shilpibouddha/PycharmProjects/app2-portfolio/StudentProjectDay22/images (2)"+ row["image"])
 
 with col3:
     for index, row in df[8:].iterrows():
         st.subheader(f"{row["first name"].title()} {row["last name"].title()}")
         st.write(row["role"])
-        st.image("images (2)/"+ row["image"])
+        st.image("/Users/shilpibouddha/PycharmProjects/app2-portfolio/StudentProjectDay22/images (2)/"+ row["image"])
